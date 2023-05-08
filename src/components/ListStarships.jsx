27c48d4c0aa72e1d483aff94333541d8c7a3c61c
marshalljordan1 <jordan_marshall@live.co.uk>
2023-05-08 12:53:18 +0200

    Add infinite scroll

--- src/components/ListStarships.jsx
+++ src/components/ListStarships.jsx
@@ -1,13 +1,12 @@
-import { useContext, useState } from "react";
+import { React, useContext, useEffect, useState } from "react";
 import { ShipListContext } from "../context/ShipListContext";
-import ShipDropdown from "./ShipDropdown";
+import ShipDropdown from "../components/ShipDropdown";
 
 const ListStarships = () => {
   const shipList = useContext(ShipListContext);
-
   const [selectedShip, setSelectedShip] = useState(null);
 
-  const handleShipClick = (ship) => {
+  const handleClick = (ship) => {
     setSelectedShip(ship);
   };
 
@@ -15,21 +14,24 @@ const ListStarships = () => {
     <>
       {shipList ? (
         <>
-          {shipList.map((ship) => (
-            <div
-              onClick={() => handleShipClick(ship)}
-              className="list-container"
-              key={ship.url}
-            >
-              <ul className="starship-list" key={ship.url}>
-                <li className="list-name">{ship.name.toUpperCase()}</li>
-                <li className="list-model">{ship.model.toUpperCase()}</li>
-              </ul>
-              {selectedShip && selectedShip.name === ship.name && (
-                <ShipDropdown shipInfo={selectedShip} />
-              )}
-            </div>
-          ))}
+          {shipList.map((ship) => {
+            return (
+              <div
+                onClick={() => handleClick(ship)}
+                className="list-container"
+                key={ship.name}
+              >
+                <ul className="starship-list">
+                  <li className="list-name">{ship.name}</li>
+                  <li className="list-model">{ship.model}</li>
+                </ul>
+
+                {selectedShip === ship && (
+                  <ShipDropdown shipInfo={selectedShip} />
+                )}
+              </div>
+            );
+          })}
         </>
       ) : (
         <p className="text">Loading...</p>
--- src/components/styled-components/StyledShipDropdown.jsx
+++ src/components/styled-components/StyledShipDropdown.jsx
@@ -3,3 +3,5 @@ import ShipDropdown from "../ShipDropdown";
 const StyledShipDropdown = styled(ShipDropdown)`
   border: red solid;
 `;
+
+export default StyledShipDropdown;
